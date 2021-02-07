24d502f70539e150f43b05338f37855b7e234218
sr.andreev <sr.andreev@tensor.ru>
2021-02-07 11:59:55 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = new Map();
+  for (const [ open, close ] of bracketsConfig) {
+    config.set(close, open);
+  }
+
+  const waiting = [];
+  for (const char of str) {
+    if (config.has(char)) {
+      if (char === config.get(char)) {
+        if (waiting[waiting.length - 1] === char) {
+          waiting.pop();
+        } else {
+          waiting.push(char);
+        }
+      } else {
+        if (waiting.pop() !== config.get(char)) {
+          return false;
+        }
+      }
+    } else {
+     waiting.push(char);
+    }
+  }
+
+  return waiting.length === 0;
 }
